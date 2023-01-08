006a2ba76680f440f343447fc82827d8712e1fab
CoderTheArka <117577423+CoderTheArka@users.noreply.github.com>
2023-01-08 09:49:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("MY FIRST CONSOLE PROJECT")
+
+var name='Ranbir '
+console.log(name)
+
+var name1='Kapoor'
+console.log(name1)
+
+var name12 = name.concat(name1);
+console.log(name12);
+
+var a = 10;
+var b = 5;
+
+var add= a + b;
+console.log(add);
+
+c=8;
+d=3;
+
+var subtract= c-d;
+console.log(subtract);
+
+e=5;
+f=10;
+
+var mul = e*f;
+console.log(mul);
+
+g=4;
+h=2;
+
+var div= g/h;
+console.log(div);
